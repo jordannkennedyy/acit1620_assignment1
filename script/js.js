8b3a0aa90c182dafb3d7b84bbdc36575def52887
Jordan Kennedy <jkennedy107@my.bcit.ca>
2023-03-27 20:06:17 -0700

    cancel button hides textarea, save, cancel

--- script/js.js
+++ script/js.js
@@ -1,6 +1,9 @@
+// Text on the button should be updated to “Light Theme”.
+// Pressing the “Light Theme” button would reverse all the changes. (3 points)
+
+
+
 const darkmodebutton = document.querySelector('.dark_theme')
-
-
 
 function darkmode(){
     var body = document.querySelector("body")
@@ -18,13 +21,18 @@ function darkmode(){
     aside.classList.toggle("dark_mode");
     darkmodebutton.textContent === "light theme";
 }
-darkmodebutton.addEventListener("click", darkmode)
+darkmodebutton.addEventListener("click", darkmode);
 
 
-// When the “Dark Theme” button is clicked,
-// Background colors should change to darker colors (use 2 different colors for the left and right parts)
-// Text colors on darker backgrounds should be updated to have good contrast
-// Use toggling class properties to achieve the two previous changes
-// Don't add css code in JS (don't use .style property)
-// Text on the button should be updated to “Light Theme”.
-// Pressing the “Light Theme” button would reverse all the changes. (3 points)
+
+
+// Clicking the “cancel” button should hide the text area, save, and cancel buttons.
+
+const cancelbutton = document.querySelector(".cancel")
+
+function cancelfeature(){
+    document.querySelector("textarea").hidden = true;
+    document.querySelector(".cancel").hidden = true;
+    document.querySelector(".save").hidden = true;
+}
+cancelbutton.addEventListener("click", cancelfeature);
